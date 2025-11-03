24aab23bee61030a07bc0d7d7d4ea319291d8870
theRealArbab <arbabqadir35@gmail.com>
2025-11-03 23:15:34 +0500

    Update music_library.py

--- music_library.py
+++ music_library.py
@@ -7,10 +7,9 @@ music = {
     "summer": "https://open.spotify.com/track/27x2IrIGwr56QWkqJ4cu9I?si=02d906f3da584316",
     "king": "https://open.spotify.com/track/4RpWyOGMA0P4BE4MYBvVGT?si=001cf07c833246b6",
     'meaning': 'https://open.spotify.com/track/3ucfniv4fLB3RPA6N9iLM2?si=b2525be075c741b9',
-    'lilac album': 'https://open.spotify.com/album/01dPJcwyht77brL4JQiR8R?si=UL9vwwXQSJCf-7K1m9u67Q',
-    'palette album': 'https://open.spotify.com/album/5V8n6fqyAPxvFTibPhQVcp?si=vR-kkroSR16gkugMlUTkQw',
     'fox': 'https://open.spotify.com/track/4hIFYVCmb7PSzG9wf1JkEP?si=214d22941f6f49e6',
     'river of three crossings': 'https://open.spotify.com/track/7DPsTowE1RzauXo1KcdWYh?si=d3e8bee6602547b2'
+
 
 
 }
